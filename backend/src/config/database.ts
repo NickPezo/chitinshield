13c07a336a8172ff93c2c5499b9d4f507f3e0cb4
NickPezo <167672611+NickPezo@users.noreply.github.com>
2024-07-01 23:16:05 -0500

    Update database.ts

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -5,10 +5,10 @@ module.exports = {
     charset: "utf8mb4",
     collate: "utf8mb4_bin"
   },
-  dialect: process.env.DB_DIALECT || "mysql",
+  dialect: process.env.DB_DIALECT || "postgres",  // Asegúrate de que el dialecto predeterminado sea postgres
   timezone: "-03:00",
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 5432,  // Asegúrate de que el puerto predeterminado sea 5432 para PostgreSQL
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
